b0243f0ecc07d8acd336a7b1daa41754c9e4daf8
phantony <32271574+PhantonyGit@users.noreply.github.com>
2020-10-15 22:58:12 +0700

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.tmlab;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +9,12 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText UsernameField;
-    EditText PassField;
-    RadioGroup RadioGroup;
-    RadioButton MaleButton, FemaleButton;
-    Button LoginButton;
+    EditText UsernameField, PassField;
+    Button LoginButton, RegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,38 +23,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         UsernameField = findViewById(R.id.UsernameField);
         PassField = findViewById(R.id.PassField);
-        RadioGroup = findViewById(R.id.RadioGroup);
-        MaleButton = findViewById(R.id.MaleButton);
-        FemaleButton = findViewById(R.id.FemaleButton);
         LoginButton = findViewById(R.id.LoginButton);
+        RegisterButton = findViewById(R.id.RegisterButton);
         LoginButton.setOnClickListener(this);
-    }
 
-    @Override
-    public void onClick(View v) {
-        ValidasiData();
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.register);
+            }
+        });
+
     }
 
     public void ValidasiData () {
         Editable Username = UsernameField.getText();
         Editable Password = PassField.getText();
-        String Gender = "";
 
-        if (TextUtils.isEmpty(Username) && TextUtils.isEmpty(Password)){
+        if (TextUtils.isEmpty(Username) || TextUtils.isEmpty(Password)){
             UsernameField.setError("Tidak Boleh Kosong");
-        }else if (Password.length() < 8 || Password.length() > 25 ) {
+            PassField.setError("Tidak Boleh Kosong");
+        }
+        else if (Password.length() < 8 || Password.length() > 25 ) {
             Toast.makeText(this, "Password > 8 || Password = 25", Toast.LENGTH_SHORT).show();
         }else {
             AlertDialog.Builder AlertDialog = new AlertDialog.Builder(this);
             AlertDialog.setTitle("Succes");
             AlertDialog.show();
         }
+    }
 
-        if(MaleButton.isChecked()){
-            Gender = "Male";
-        }else{
-            Gender = "Female";
-        }
+    @Override
+    public void onClick(View v) {
 
     }
 
